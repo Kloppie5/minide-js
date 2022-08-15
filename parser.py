bbb30f50c8c3ed942640f61e86980e82d92bd94a
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-08-15 15:21:38 +0200

    Outsourced parsing for the time being

--- parser.py
+++ parser.py
@@ -5,13 +5,28 @@
 #
 ##
 
+import os
+from slimit.parser import Parser as SlimitParser
+
 class Parser ( object ) :
 
+    # State enum
+    STATE_START = 0
+
     def __init__ ( self ) :
+        self.state = self.STATE_START
         pass
 
     def parse ( self, code ) :
-        print ( "parse" )
+        print("parse")
+        parser = SlimitParser()
+        tree = parser.parse(code)
+        print(tree.to_ecma())
 
     def parse_file ( self, filename ) :
-        print ( "parse_file" )
+        if not os.path.isfile(filename) :
+            print("File not found: " + filename)
+            return
+        with open( filename, "r" ) as f :
+            code = f.read()
+        self.parse(code)
